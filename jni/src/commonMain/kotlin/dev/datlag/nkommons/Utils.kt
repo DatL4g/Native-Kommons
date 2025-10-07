79a6354779371414291452836af27c6d4ba27b86
Jeff Retz <jeff.retz08@gmail.com>
2025-10-07 20:25:40 +0200

    better common methods

--- jni/src/commonMain/kotlin/dev/datlag/nkommons/Utils.kt
+++ jni/src/commonMain/kotlin/dev/datlag/nkommons/Utils.kt
@@ -6,11 +6,19 @@ import dev.datlag.nkommons.binding.jstring
 import kotlinx.cinterop.ByteVar
 import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.invoke
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.pointed
 import kotlinx.cinterop.toKStringFromUtf8
+import kotlinx.cinterop.usePinned
 import kotlinx.cinterop.wcstr
+
+@OptIn(ExperimentalForeignApi::class)
+fun CPointer<JNIEnvVar>.newString(chars: CPointer<jcharVar>, length: Int): jstring? {
+    val method = pointed.pointedCommon?.NewString ?: return null
+    return method.invoke(this, chars, length)
+}
 
 @OptIn(ExperimentalForeignApi::class)
 fun String.toJstring(env: CPointer<JNIEnvVar>): jstring? = memScoped {
@@ -18,22 +26,29 @@ fun String.toJstring(env: CPointer<JNIEnvVar>): jstring? = memScoped {
 }
 
 @OptIn(ExperimentalForeignApi::class)
-expect fun jstring.getStringUTFChars(env: CPointer<JNIEnvVar>): CPointer<ByteVar>?
+fun jstring.getStringUTFChars(env: CPointer<JNIEnvVar>): CPointer<ByteVar>? {
+    val method = env.pointed.pointedCommon?.GetStringUTFChars ?: return null
+    return method.invoke(env, this, null)
+}
 
 @OptIn(ExperimentalForeignApi::class)
-fun jstring.getString(env: CPointer<JNIEnvVar>): String? {
+fun jstring.toKString(env: CPointer<JNIEnvVar>): String? {
     val chars = getStringUTFChars(env)
     return chars?.toKStringFromUtf8()
 }
 
 @OptIn(ExperimentalForeignApi::class)
-expect fun CPointer<JNIEnvVar>.newIntArray(size: Int): jintArray?
+fun CPointer<JNIEnvVar>.newIntArray(size: Int): jintArray? {
+    val method = pointed.pointedCommon?.NewIntArray ?: return null
+    return method.invoke(this, size)
+}
 
 @OptIn(ExperimentalForeignApi::class)
-expect fun CPointer<JNIEnvVar>.fill(target: jintArray, value: IntArray): jintArray?
-
-@OptIn(ExperimentalForeignApi::class)
-fun CPointer<JNIEnvVar>.newString(chars: CPointer<jcharVar>, length: Int): jstring? {
-    val method = pointed.pointedCommon?.NewString ?: return null
-    return method.invoke(this, chars, length)
+fun CPointer<JNIEnvVar>.fill(target: jintArray, value: IntArray): jintArray? {
+    val method = pointed.pointedCommon?.SetIntArrayRegion ?: return null
+    value.usePinned { pinnedArray ->
+        val pointer = pinnedArray.addressOf(0)
+        method.invoke(this, target, 0, value.size, pointer)
+    }
+    return target
 }
--- jni/src/desktopMain/kotlin/dev/datlag/nkommons/Utils.desktop.kt
+++ jni/src/desktopMain/kotlin/dev/datlag/nkommons/Utils.desktop.kt
@@ -1,38 +1,7 @@
 package dev.datlag.nkommons
 
-import dev.datlag.nkommons.binding.jcharVar
-import dev.datlag.nkommons.binding.jintArray
 import dev.datlag.nkommons.binding.jobject
-import dev.datlag.nkommons.binding.jstring
-import kotlinx.cinterop.ByteVar
-import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.addressOf
-import kotlinx.cinterop.invoke
-import kotlinx.cinterop.pointed
-import kotlinx.cinterop.usePinned
-
-@OptIn(ExperimentalForeignApi::class)
-actual fun jstring.getStringUTFChars(env: CPointer<JNIEnvVar>): CPointer<ByteVar>? {
-    val method = env.pointed.pointed?.GetStringUTFChars ?: return null
-    return method.invoke(env, this.forPlatform(), null)
-}
-
-@OptIn(ExperimentalForeignApi::class)
-actual fun CPointer<JNIEnvVar>.newIntArray(size: Int): jintArray? {
-    val method = pointed.pointed?.NewIntArray ?: return null
-    return method.invoke(this, size)
-}
-
-@OptIn(ExperimentalForeignApi::class)
-actual fun CPointer<JNIEnvVar>.fill(target: jintArray, value: IntArray): jintArray? {
-    val method = pointed.pointed?.SetIntArrayRegion ?: return null
-    value.usePinned { pinnedArray ->
-        val pointer = pinnedArray.addressOf(0)
-        method.invoke(this, this.forPlatform(), 0, value.size, pointer)
-    }
-    return target
-}
 
 /**
  * Casts common jobject to jni.jobject

69f11987ab8e399da8e3e0bcdd7dac671a531495
Jeff Retz <jeff.retz08@gmail.com>
2025-10-08 15:22:18 +0200

    added encoding option for string utils

--- jni/src/commonMain/kotlin/dev/datlag/nkommons/Utils.kt
+++ jni/src/commonMain/kotlin/dev/datlag/nkommons/Utils.kt
@@ -1,8 +1,8 @@
 package dev.datlag.nkommons
 
 import dev.datlag.nkommons.binding.jboolean
+import dev.datlag.nkommons.binding.jbooleanVar
 import dev.datlag.nkommons.binding.jchar
-import dev.datlag.nkommons.binding.jcharVar
 import dev.datlag.nkommons.binding.jintArray
 import dev.datlag.nkommons.binding.jsize
 import dev.datlag.nkommons.binding.jstring
@@ -11,38 +11,31 @@ import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.invoke
-import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.pointed
 import kotlinx.cinterop.toBoolean
 import kotlinx.cinterop.toByte
 import kotlinx.cinterop.toKStringFromUtf8
 import kotlinx.cinterop.usePinned
-import kotlinx.cinterop.wcstr
 
 val JNINativeInterface.common
     get() = CommonJNINativeInterface(this)
 
 @OptIn(ExperimentalForeignApi::class)
-fun CPointer<JNIEnvVar>.newString(chars: CPointer<jcharVar>, length: Int): jstring? {
-    val method = pointed.pointedCommon?.NewString ?: return null
-    return method.invoke(this, chars, length)
+fun String.toJString(env: CPointer<JNIEnvVar>, encoding: Encoding = Encoding.UTF8): jstring? {
+    return with(encoding) {
+        this@toJString.toJString(env)
+    }
 }
 
 @OptIn(ExperimentalForeignApi::class)
-fun String.toJString(env: CPointer<JNIEnvVar>): jstring? = memScoped {
-    env.newString(wcstr.ptr, length)
-}
-
-@OptIn(ExperimentalForeignApi::class)
-fun jstring.getStringUTFChars(env: CPointer<JNIEnvVar>): CPointer<ByteVar>? {
-    val method = env.pointed.pointedCommon?.GetStringUTFChars ?: return null
-    return method.invoke(env, this, null)
-}
-
-@OptIn(ExperimentalForeignApi::class)
-fun jstring.toKString(env: CPointer<JNIEnvVar>): String? {
-    val chars = getStringUTFChars(env)
-    return chars?.toKStringFromUtf8()
+fun jstring.toKString(
+    env: CPointer<JNIEnvVar>,
+    encoding: Encoding = Encoding.UTF8,
+    isCopy: CPointer<jbooleanVar>? = null
+): String? {
+    return with(encoding) {
+        this@toKString.toKString(env, isCopy)
+    }
 }
 
 @OptIn(ExperimentalForeignApi::class)
